a99e8e34e765a0cd5c4b39ae0908035abd3561ad
Matheus Farias <matheusfariasdelima@gmail.com>
2024-02-23 11:49:20 -0300

    Aula 01.3

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const btn1 = document.queryselector('button')
 const listaFilmes = document.querySelector('#listaFilmes')
 //cria o elemento li
 const itemLista = document.createElement('li')
+//pega o input 
+const textoUsuario = document.querySelector('#filmeInput')
 
 btn1.addEventListener('click',()=>{
     //adiciona um li a lista
