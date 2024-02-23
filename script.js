133026494129b8140e0e8f862378ad19e50891fc
Matheus Farias <matheusfariasdelima@gmail.com>
2024-02-23 11:42:25 -0300

    Aula 01.1

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btn1 = document.queryselector('button')
+//pega a lista de filmes
 const listaFilmes = document.querySelector('#listaFilmes')
+//cria o elemento li
+const itemLista = document.createElement('li')
 
 btn1.addEventListener('click',()=>{
-    windown.alert("Clicou!")
+    
 })
