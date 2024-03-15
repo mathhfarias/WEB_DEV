5d87134de70e7a56734cc17270daffa410cb58a0
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-15 11:47:41 -0300

    Finalizando a aula

---
+++ AULA 02/primeiro-site-react/src/Componentes/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header>
+        <h1 className="titulo-banner">Titulo do meu banner</h1>
+        <p>Paragrafo do meu banner</p>
+        </header>
+    )
+}
+
+export default Banner
